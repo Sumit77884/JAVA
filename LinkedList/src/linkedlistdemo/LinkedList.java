f4f0301fc4c57b46b70a7ae9a78086958aeb3dcd
Sumit Kumar <sumitsingh1999220@gmail.com>
2026-08-20 00:43:27 +0530

    Add files via upload

---
+++ LinkedList/src/linkedlistdemo/LinkedList.java
@@ -0,0 +1,11 @@
+package linkedlistdemo;
+import java.util.*;
+public class LinkedListDemo{
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LinkedList<Integer>al1=new LinkedList<>();
+        LinkedList<Integer>al2=new LinkedList<>(List.of(10,20,30,40,50,60,70,80,90));
+        
+    }
+    
+}
